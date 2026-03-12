c41393f43ffd2093f15eecb65ae634eacd130c68
Bishwanath <bishwanathh20@gmail.com>
2026-03-12 21:53:46 +0530

    Update database user in fix_env_encoding.py

--- adaptive-engine/scripts/fix_env_encoding.py
+++ adaptive-engine/scripts/fix_env_encoding.py
@@ -1,8 +1,9 @@
 import pathlib
 
 p = pathlib.Path(r'c:/Users/Dell/Downloads/adaptive-engine/adaptive-engine/.env')
-text = 'MONGO_URI=mongodb+srv://patrabishwanath20_db_user:xxxx@cluster0.twvgrnr.mongodb.net/adaptive_engine?appName=Cluster0\nDB_NAME=adaptive_engine\nOPENAI_API_KEY=sk_OPEN_AI_KEY\n'
+text = 'MONGO_URI=mongodb+srv://username_db_user:xxxx@cluster0.twvgrnr.mongodb.net/adaptive_engine?appName=Cluster0\nDB_NAME=adaptive_engine\nOPENAI_API_KEY=sk_OPEN_AI_KEY\n'
 p.write_text(text, encoding='utf-8')
 print('wrote', p.read_bytes()[:20])
+
 
 
