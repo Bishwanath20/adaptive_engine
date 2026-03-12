c80290651744f81d42f1965706331a7edd5985e5
Bishwanath <bishwanathh20@gmail.com>
2026-03-12 21:41:19 +0530

    Mask sensitive data in fix_env_encoding.py
    Updated sensitive information in the .env file to use 'xxxx' for the password.

--- adaptive-engine/scripts/fix_env_encoding.py
+++ adaptive-engine/scripts/fix_env_encoding.py
@@ -1,6 +1,7 @@
 import pathlib
 
 p = pathlib.Path(r'c:/Users/Dell/Downloads/adaptive-engine/adaptive-engine/.env')
-text = 'MONGO_URI=mongodb+srv://patrabishwanath20_db_user:bishwanath20@cluster0.twvgrnr.mongodb.net/adaptive_engine?appName=Cluster0\nDB_NAME=adaptive_engine\nOPENAI_API_KEY=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\n'
+text = 'MONGO_URI=mongodb+srv://patrabishwanath20_db_user:xxxx@cluster0.twvgrnr.mongodb.net/adaptive_engine?appName=Cluster0\nDB_NAME=adaptive_engine\nOPENAI_API_KEY=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\n'
 p.write_text(text, encoding='utf-8')
 print('wrote', p.read_bytes()[:20])
+
